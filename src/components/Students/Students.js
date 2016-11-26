9506616ca062c72d8cdac6ab64a90e027605c0dd
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-26 16:27:28 -0500

    Prepares for additional component logic

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Students.css';
-
 import Student from '../Student/Student';
 
-const Students = props => {
-    return (
-        <ul className="Students">
-            {
-                ( props.students  ) ?
-                    Object
-                        .keys(props.students)
-                        .map(key =>
-                            <Student
-                                key={key}
-                                name={props.students[key].name}
-                                grade={props.students[key].grade} />
-                        )
-                    : <li>No students.</li>
-            }
-        </ul>
-    )
-};
+//Handle delete student
+
+class Students extends Component {
+    render() {
+        const { students } = this.props;
+
+        return (
+            <ul className="Students">
+                {
+                    ( students  ) ?
+                        Object
+                            .keys( students )
+                            .map( key =>
+                                <Student
+                                    key={ key }
+                                    name={ students[key].name }
+                                    grade={ students[key].grade } />
+                            )
+                        : <li>No students.</li>
+                }
+            </ul>
+        )
+
+    }
+
+}
+
 
 Students.propTypes = {
     students: React.PropTypes.object
