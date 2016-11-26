269fed5bd7b49f3542912cf19017300249e6050e
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-26 15:47:38 -0500

    Adds a very, very basic render test

---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Summary.css';
+import { min, max, avg } from '../../helpers/stats';
+
+const Summary = props => {
+    return (
+        <div className="Summary">
+            <span>Metrics</span>
+            <div className="Summary-min">
+                <span>Min - {min(props.students)}</span>
+
+            </div>
+            <div className="Summary-average">
+                <span>Avg - {avg(props.students)}</span>
+            </div>
+            <div className="Summary-max">
+                <span>Max - {max(props.students)}</span>
+            </div>
+        </div>
+    )
+};
+
+Summary.propTypes = {
+    students: React.PropTypes.object.isRequired
+};
+
+export default Summary;
