3ba5558312fa88ea7426faf2428d4e372bc6b2c7
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-26 14:00:46 -0500

    Add catch for no students passed in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import sampleData from '../../data/demo.js';
 import Header from '../Header/Header';
 import Intro from '../Intro/Intro';
-import Student from '../Student/Student';
+import Students from '../Students/Students';
 
 class App extends Component {
 
@@ -20,17 +20,7 @@ class App extends Component {
             <div className="App">
                 <Header text="React Gradebook" />
                 <Intro text="Exam Summary" />
-                <ul className="Students">
-                    {
-                        Object
-                            .keys(this.state.students)
-                            .map(key =>
-                                <Student key={key}
-                                         name={this.state.students[key].name}
-                                         grade={this.state.students[key].grade} />
-                            )
-                    }
-                </ul>
+                <Students students={this.state.students}/>
 
             </div>
         );
---
+++ src/components/Students/Students.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Students.css';
+
+import Student from '../Student/Student';
+
+const Students = props => {
+    return (
+        <ul className="Students">
+            {
+                ( props.students  ) ?
+                    Object
+                        .keys(props.students)
+                        .map(key =>
+                            <Student
+                                key={key}
+                                name={props.students[key].name}
+                                grade={props.students[key].grade} />
+                        )
+                    : <li>No students.</li>
+            }
+        </ul>
+    )
+};
+
+export default Students;
