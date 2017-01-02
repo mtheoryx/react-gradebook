b560b7ee1ba9bd60e8eb5aa0d80cb06b6e83174b
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-02 16:02:27 -0500

    Adds action testing to delete button

--- src/components/DeleteButton/DeleteButton.test.js
+++ src/components/DeleteButton/DeleteButton.test.js
@@ -2,6 +2,8 @@
 import React from 'react';
 //noinspection JSUnresolvedVariable
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import DeleteButton from './DeleteButton';
 
@@ -10,4 +12,14 @@ describe('DeleteButton component', () => {
         const div = document.createElement('div');
         ReactDOM.render(<DeleteButton itemId={'student1'} onDelete={() => {}}/>, div);
 	});
+	it('handles a click event', () => {
+		const onButtonClick = sinon.spy();
+		const Btn = shallow(
+			<DeleteButton onDelete={onButtonClick}  itemId={'student1'}/>
+		);
+		Btn.find('button').simulate('click');
+		expect(onButtonClick.calledOnce).toEqual(true);
+	});
+
 });
+
