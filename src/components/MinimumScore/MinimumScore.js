738261518f82b876e3b3c0fe3fc2882d3f59055c
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 17:09:34 -0500

    MinimumScore to stateless

--- src/components/MinimumScore/MinimumScore.js
+++ src/components/MinimumScore/MinimumScore.js
@@ -3,12 +3,10 @@ import './MinimumScore.css';
 import { min } from '../../helpers/stats';
 import { getGradesFromObject } from '../../helpers/grades';
 
-const MinimumScore = ({ students }) => {
-    return (
-        <div className="MinimumScore">
-            <span>Min - { min(getGradesFromObject(students)) } %</span>
-        </div>
-    )
-};
+const MinimumScore = ({ students }) => (
+    <div className="MinimumScore">
+        <span>Min - { min(getGradesFromObject(students)) } %</span>
+    </div>
+);
 
 export default MinimumScore;
