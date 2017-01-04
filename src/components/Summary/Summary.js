4d7f869be84cf0f25283bce8083788a131c4d381
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 17:10:24 -0500

    Summary to stateless function

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -4,17 +4,15 @@ import MinimumScore from '../MinimumScore/MinimumScore';
 import AverageScore from '../AverageScore/AverageScore';
 import MaximumScore from '../MaximumScore/MaximumScore';
 
-const Summary = ({ students }) => {
-    return (
-        <div className="Summary">
-            <span>Metrics</span>
+const Summary = ({ students }) => (
+    <div className="Summary">
+        <span>Metrics</span>
 
-            <MinimumScore students={ students } />
-            <AverageScore students={ students } />
-            <MaximumScore students={ students } />
-        </div>
-    )
-};
+        <MinimumScore students={ students } />
+        <AverageScore students={ students } />
+        <MaximumScore students={ students } />
+    </div>
+);
 
 Summary.propTypes = {
     students: React.PropTypes.object.isRequired
