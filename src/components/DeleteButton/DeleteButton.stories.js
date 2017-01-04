92ff093efcbfb5fed869836da596a87ac73d06f6
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 18:15:47 -0500

    Add notes addon for storybook

--- src/components/DeleteButton/DeleteButton.stories.js
+++ src/components/DeleteButton/DeleteButton.stories.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import { WithNotes } from '@kadira/storybook-addon-notes';
+
 import DeleteButton from './DeleteButton';
 import maxWidthCentered from '../../helpers/storybook-helpers/max-width-centered';
 
+const deleteButtonNote = `
+    This positioning of the X inside is pretty poor and should be fixed.
+`;
+
 storiesOf('DeleteButton', module)
     .addDecorator(maxWidthCentered)
-    .add('Default', () => (<DeleteButton onDelete={ action('delete-button-click') } itemId={'student1'}/>));
+    .add('Default', () => (
+        <WithNotes notes={ deleteButtonNote } >
+            <DeleteButton onDelete={ action('delete-button-click') } itemId={'student1'} />
+        </WithNotes>
+    ));
