91dee872228e614f2265641a2353d2daa686b4f8
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 17:06:24 -0500

    AverageScore to stateless function

--- src/components/AverageScore/AverageScore.js
+++ src/components/AverageScore/AverageScore.js
@@ -3,12 +3,10 @@ import './AverageScore.css';
 import { avg } from '../../helpers/stats';
 import { getGradesFromObject } from '../../helpers/grades';
 
-const AverageScore = ({ students }) => {
-    return (
-        <div className="AverageScore">
-            <span>Avg - { avg(getGradesFromObject(students)) } %</span>
-        </div>
-    )
-};
+const AverageScore = ({ students }) => (
+    <div className="AverageScore">
+        <span>Avg - { avg(getGradesFromObject(students)) } %</span>
+    </div>
+);
 
 export default AverageScore;
