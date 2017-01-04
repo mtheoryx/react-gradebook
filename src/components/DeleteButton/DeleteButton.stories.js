397f1fb3d582b9f41a77bed7d47ea5713b1f2e05
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 17:40:16 -0500

    Adds action logging to delete button click

--- src/components/DeleteButton/DeleteButton.stories.js
+++ src/components/DeleteButton/DeleteButton.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf, action } from '@kadira/storybook';
 import DeleteButton from './DeleteButton';
 import maxWidthCentered from '../../helpers/storybook-helpers/max-width-centered';
 
 storiesOf('DeleteButton', module)
     .addDecorator(maxWidthCentered)
-    .add('Default', () => (<DeleteButton onDelete={function mock() {}} itemId={'student1'}/>));
+    .add('Default', () => (<DeleteButton onDelete={ action('delete-button-click') } itemId={'student1'}/>));
