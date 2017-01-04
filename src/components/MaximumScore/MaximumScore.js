9a839b489f6e065d79f0d634ba4d8fca3ff56e89
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-04 17:08:16 -0500

    Max score to stateless function, fix css import

--- src/components/MaximumScore/MaximumScore.js
+++ src/components/MaximumScore/MaximumScore.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import MaximumScore from './MaximumScore.css';
+import './MaximumScore.css';
 import { max } from '../../helpers/stats';
 import { getGradesFromObject } from '../../helpers/grades';
 
-const AverageScore = ({ students }) => {
-    return (
-        <div className="MaximumScore">
-            <span>Max - { max(getGradesFromObject(students)) } %</span>
-        </div>
-    )
-};
+const MaximumScore = ({ students }) => (
+    <div className="MaximumScore">
+        <span>Max - { max(getGradesFromObject(students)) } %</span>
+    </div>
+);
 
-export default AverageScore;
+export default MaximumScore;
