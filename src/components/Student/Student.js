75d8491ef41b6a414cb0226cc7e24f02087e347e
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-07 11:09:13 -0500

    Student to stateless component

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './Student.css';
-class Student extends React.Component {
 
-    render() {
-        const { student } = this.props;
-
-        return (
-            <div className="Student">
-                { student.name } - { student.grade }
-            </div>
-        )
-    }
-}
+const Student = ({ student }) => (
+    <div className="Student">
+        { student.name } - { student.grade }
+    </div>
+);
 
 Student.propTypes = {
     student: React.PropTypes.object.isRequired
