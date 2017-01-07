ca2f916e09a409494c987b5394021f485bcc4459
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-07 11:10:55 -0500

    Students to stateless component

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,42 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Students.css';
 import Student from '../Student/Student';
 import DeleteButton from '../DeleteButton/DeleteButton';
 import AddStudent from '../AddStudent/AddStudent';
 
-class Students extends Component {
-    render() {
-        const { students, deleteStudent, addStudent } = this.props;
-
-        return (
-            <div className="Students">
-                <AddStudent addStudent={ addStudent }/>
-                <br/>
-                {
-                    ( students )
-                        ? Object
-                            .keys( students )
-                            .map( key =>
-                            <div key={ key } className="student-entry">
-                                <Student
-                                    studentId={ key }
-                                    student={ students[key] }
-                                    deleteStudent={ deleteStudent }
-                                />
-                                <DeleteButton
-                                    onDelete={ deleteStudent }
-                                    itemId={ key }/>
-                            </div>
-                            )
-                        : <div>No students.</div>
-                }
-            </div>
-        )
-
-    }
-
-}
-
+const Students = ({ students, deleteStudent, addStudent }) => (
+    <div className="Students">
+        <AddStudent addStudent={ addStudent }/>
+        <br/>
+        {
+            ( students )
+                ? Object
+                    .keys( students )
+                    .map( key =>
+                    <div key={ key } className="student-entry">
+                        <Student
+                            studentId={ key }
+                            student={ students[key] }
+                            deleteStudent={ deleteStudent }
+                        />
+                        <DeleteButton
+                            onDelete={ deleteStudent }
+                            itemId={ key }/>
+                    </div>
+                    )
+                : <div>No students.</div>
+        }
+    </div>
+);
 
 Students.propTypes = {
     students: React.PropTypes.object
