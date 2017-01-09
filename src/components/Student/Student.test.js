25021fc1afcc646085ef9bdcdf9a73c7edba8a93
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-09 17:44:54 -0500

    Test edit student name

--- src/components/Student/Student.test.js
+++ src/components/Student/Student.test.js
@@ -25,3 +25,24 @@ test('Student name edit function called with correct arguments', () => {
     expect(updateStudent).toBeCalledWith('student1', studentUpdate);
 });
 
+test('Student grade edit function called with correct arguments', (() => {
+    const updateStudent = jest.fn();
+
+    const student = {
+        name: 'Bill Someone',
+        grade: 85
+    };
+    const studentUpdate = {
+        name: 'Bill Someone',
+        grade: 100
+    };
+    const wrapper = mount(
+        <Student student={ student } studentId={'student1'} updateStudent={ updateStudent }/>
+    );
+    const gradeInput = wrapper.find('.editGradeInput');
+
+    gradeInput.simulate('change', {target: {name: 'grade', value: '100'}});
+
+    expect(updateStudent).toBeCalledWith('student1', studentUpdate);
+
+}));
