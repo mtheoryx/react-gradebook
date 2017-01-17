ae31766cf7190db224454ac87f55096120e4f58e
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-16 19:02:40 -0500

    Initial add and try at aphrodite

--- src/components/DeleteButton/DeleteButton.js
+++ src/components/DeleteButton/DeleteButton.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import './DeleteButton.css';
+import { StyleSheet, css } from 'aphrodite';
 
 const DeleteButton = ({ onDelete, itemId }) => {
     const onHandleClick = () => onDelete(itemId);
 
     return (
         <button
-            className="DeleteButton"
+            className={css(styles.deleteButton)}
             onClick={ onHandleClick }>
-            &nbsp;
         </button>
 )};
+
+const styles = StyleSheet.create({
+    deleteButton: {
+        height: '40px',
+        width: '40px',
+        border: '1px solid #ccc',
+        backgroundColor: '#fafafa',
+        position: 'relative',
+        cursor: 'pointer',
+        ':hover': {
+            backgroundColor: '#ececec',
+            borderColor: '#7b7b7b'
+        },
+        ':active': {
+            outline: 'none'
+        },
+        ':focus': {
+            outline: 'none'
+        },
+        ':after': {
+            content: '"\u00d7"',
+            fontSize: '40px',
+            color: '#505050',
+            position: 'absolute',
+            top: '-8px',
+            left: '7px'
+        }
+    }
+});
 
 DeleteButton.propTypes = {
     onDelete: React.PropTypes.func.isRequired,
